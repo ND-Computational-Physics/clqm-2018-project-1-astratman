ad6e20d228c1a70a3b69b666ffd144bf970e3972
astratman <astratma@nd.edu>
2018-01-30 09:35:35 -0500

    Linear Schrodinger solver steps

---
+++ LinearShrodingerSolver.py
@@ -0,0 +1,28 @@
+"""Solves Schrodinger equation
+
+Anne Stratman
+Ben Riordan
+Jan. 25th, 2018
+Computational Lab in Quantum Mechanics
+
+Steps:
+1. Transform radial Schrodinger equation to a dimensionless? form
+2. Rewrite as a matrix eigenvalue problem
+	a. Set up array of x-values
+	b. Set up equation for potential and evaluate potential at x-values to generate array of potential values
+	c. Construct 1D array of diagonal matrix elements
+	d. Construct 1D array of off-diagonal matrix elements
+	e. Construct matrix
+	f. Use numpy eigensolver to diagonalize matrix
+3. Results: eigenvectors are states, eigenvalues are energies
+4. Write output file with energies and eigenvectors as rows
+
+Tests:
+1. Infinite square well
+
+
+"""
+import numpy as np
+
+"""Define variables"""
+
