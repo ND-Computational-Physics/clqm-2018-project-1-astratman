83483ff434124960976a4f93cb80ea5d536551f7
Unknown <astratma@nd.edu>
2018-02-14 19:52:52 -0500

    Auto stash before merge of "master" and "origin/master"

--- LinearSchrodingerSolver.py
+++ LinearSchrodingerSolver.py
@@ -283,8 +283,8 @@ if (__name__ == "__main__"):
         return -(1/2)*x**2/0.511
      
     #run(ho_potential, 0, 1, 100, 0.511, 1, x_points = True, e_values = True, e_vectors = True, plot = False)
-    print('buffer line')
-    run(ho_potential, -1, 1, 100, 0.511, 1, solver = 2, x_points = True, e_values = True, e_vectors = True)
+    #print('buffer line')
+    #run(ho_potential, -1, 1, 100, 0.511, 1, solver = 2, x_points = True, e_values = True, e_vectors = True
 
 
 
